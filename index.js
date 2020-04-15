7fa484be3a8679448c83a8d3d5f09e13cd798afb
Alli Colyer <allicolyer@gmail.com>
2020-04-15 12:01:18 -0700

    Added oAuth code, add sample env
    Co-authored-by: Andrij Radio <driijko@gmail.com>

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-const credentials = {
-  client: {
-    id: process.env.OAUTHID,
-    secret: process.env.OAUTHSECRET
-  },
-  auth: {
-    tokenHost: 'https://www.recurse.com/oauth/authorize'
-  }
-};
- 
-const oauth2 = require('simple-oauth2').create(credentials);
-
 // Express Initialize
 const app = express();
 // Express Uses CORS
 app.use(cors());
 
 // Set up our server
-app.listen(process.env.PORT || 8000,()=> {
+app.listen(process.env.PORT || 8000, () => {
   console.log('listen port 8000');
 })
 
@@ -45,17 +33,67 @@ const data = {
   }
 }
 
-app.get('/resources', async (req, res)=> {
+app.get('/resources', async (req, res) => {
   res.send(data);
 });
 
+//creates our OAuth credientals
+const credentials = {
+  client: {
+    id: process.env.OAUTHID,
+    secret: process.env.OAUTHSECRET
+  },
+  auth: {
+    tokenHost: 'https://www.recurse.com/oauth/authorize'
+  }
+};
+
+//creates an OAuth instance
+const oauth2 = require('simple-oauth2').create(credentials);
+
+app.get("/callback", async function (req, res) {
+  console.log("/callback");
+  const tokenConfig = {
+    code: req.query.code,
+    redirect_uri: "http://localhost:8000/callback"
+  };
+
+  try {
+    const result = await oauth2.authorizationCode.getToken(tokenConfig);
+    const accessToken = oauth2.accessToken.create(result);
+    console.log(accessToken)
+    res.redirect("/account");
+  } catch (error) {
+    console.log("Access Token Error", error.message);
+    res.send("Error creating token: " + error.message);
+  }
+});
+
+//login route
+app.get("/login", async function (req, res) {
+  try {
+    const authorizationUri = oauth2.authorizationCode.authorizeURL({
+      redirect_uri: "http://localhost:8000/callback"
+    });
+
+    // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
+    res.redirect(authorizationUri);
+  } catch (error) {
+    console.log("Auth got error", error.message);
+    res.send("Error creating token: " + error.message);
+  }
+});
+
+app.get('/account', async (req, res) => {
+  res.send("welcome to your account");
+});
 
 
 // This is the data structure we will eventually use to send to our front end, but for now we'll keep it empty.
 // const data = [];
 
 // app.get('/cocktailList', async (req, res)=> {
-  
+
   // Step 0: Iterate through each type of cocktail
   // for (let i = 0 ; i < drinkNames.length ; i++) {
 
@@ -71,15 +109,15 @@ app.get('/resources', async (req, res)=> {
 
       // Step 3: Parse data
       // For a given type of drink, there can be multiple versions.
-      // The 'drinks' property is an array, 
+      // The 'drinks' property is an array,
       // each element of which contains a given version of a type of drink, like margaritas.
       // We always grab the first version as a "default" version, hence using the index '0' below.
 
       // Step 4: Update our local data structure.
       // If a given drink has less than 15 ingredients, there will be a series of null 'strIngredient' properties.
-      // Once we reach a 'null' ingredient, we break from the loop. 
+      // Once we reach a 'null' ingredient, we break from the loop.
       // Otherwise, we add it to our data object.
-  
+
 
   // Step 5: Send our data to our front-end.
   // res.send(data);
